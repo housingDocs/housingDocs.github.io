6f4c3b9c186b34162699520d5849ae704ae860a2
cookieCrood <cookie.crood@gmail.com>
2025-05-20 16:57:34 +0200

    Update Code Block tokenization, add "HTSL" icon + copy button

--- code-block.js
+++ code-block.js
@@ -2,17 +2,37 @@ function log(string) {
     console.log(`TOKENIZER > ${string}`)
 }
 
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      console.log('Copied to clipboard!');
+    })
+    .catch(err => {
+      console.error('Failed to copy: ', err);
+    });
+}
+
 function scan(string) {
-    let newString = ''
+    let newString = `<div class="page-content-code-icon">HTSL</div><button class="page-content-code-copy-button"><span class="material-symbols-outlined">content_copy</span><div class="page-content-code-copy-button-hover">Copy</div></button><hr><pre>`
     let lastPunctuation = -1
+    let isCurString = false
+
     console.log(string)
 
+    string = string + ' '
+
     for (let i = 1; i < string.length; i++) {
+
+        if (string[i] == '\t') console.log(i)
         
         const substr = string.substring(lastPunctuation + 1, i)
         let punct = string[i]
 
         let found = false
+
+        if (string[i] === '\'') {
+            if (isCurString) {}
+        }
 
         if (punctuation.includes(punct)) {
             for (const type in tokens) {
@@ -39,12 +59,15 @@ function scan(string) {
             } else if (punct !== ' ') {
                 punct = `<span class="token-punctuation">${punct}</span>`
             }
+
+            if (punct === ' ') console.log(i)
             
             newString += `${substr}${punct}`
             lastPunctuation = i
             
         }        
     }
+    newString += '</pre>'
     return newString
 }
 
@@ -53,6 +76,14 @@ const punctuation = [
     ')',
     '[',
     ']',
+    '{',
+    '}',
+    ':',
+    ';',
+    '+',
+    '-',
+    '*',
+    '/',
     ' ',
     '\n'
 ]
@@ -63,7 +94,8 @@ const tokens = {
         'in',
         'for',
         'of',
-        'import'
+        'import',
+        'is'
     ],
     "datatype": [
         'String',
@@ -77,8 +109,7 @@ const codeFields = document.querySelectorAll('.page-content-code')
 console.log(codeFields)
 
 for (const codeField of codeFields) {
-    let inner = codeField.innerText
-    inner = scan(inner)
-    console.log(inner)
-    codeField.innerHTML = inner
+    const text = codeField.querySelector('pre').textContent
+    codeField.innerHTML = scan(text)
+    codeField.querySelector('.page-content-code-copy-button').onclick = () => copyToClipboard(text)
 } 
