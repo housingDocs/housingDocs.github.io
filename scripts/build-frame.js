93061dc5535773981ef8b81d05b2a848372ea910
cookieCrood <cookie.crood@gmail.com>
2025-05-23 20:08:41 +0200

    improve frame building, add new pages, add list class

--- scripts/build-frame.js
+++ scripts/build-frame.js
@@ -8,11 +8,18 @@ const nav = {
         ]
     },
     HTSL: {
-        icon: "deployed_code",
+        icon: "code",
         iconColor: "#67C520",
         points: [
             { name: "HTSL Introduction", link: "/html/htsl/Introduction.html" },
             { name: "Code Block Editor", link: "/html/htsl/Code_Block_Editor.html"}
+        ]
+    },
+    Other: {
+        icon: "ad_group",
+        iconColor: "#F52222",
+        points: [
+            { name: "Contribute", link: "/html/other/Contribute.html" }
         ]
     }
 }
@@ -45,6 +52,24 @@ for (const group in nav) {
 }
 
 sidebars.item(0).innerHTML = navHTML
+document.querySelector('.top').innerHTML = 
+`<div class="logo">
+    <span class="material-symbols-outlined">
+        import_contacts
+    </span>
+    hDocs
+</div>
+<a href="https://discord.gg/qRvcDc3vz5"><div class="discord">
+    <img src="../../assets/discord.png">
+</div></a>`
+
+document.querySelectorAll('.page-content-list').forEach((list) => {
+    let i = 1
+    list.querySelectorAll('.page-content-list-point').forEach((point) => {
+        point.innerHTML = `<pre>  <span class="page-content-list-number">${i}.</span>  ${point.innerHTML}</pre>`
+        i++
+    })
+})
 
 
 /*<div class="sidebar-nav-group">
